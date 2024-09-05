375298df52c1dced570d60bc2383a2365205675b
Anderson Viana <anderson11viana@gmail.com>
2024-09-05 20:53:37 -0300

    update

--- src/media-server/media-server.service.ts
+++ src/media-server/media-server.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { exec } from 'child_process';
 import NodeMediaServer from 'node-media-server';
 
 @Injectable()
@@ -24,28 +23,8 @@ export class MediaServerService implements OnModuleInit {
 
     nms.on('postPublish', (id, streamPath) => {
       console.log(`Novo stream iniciado: ${streamPath}`);
-      //this.transcodeToHLS(streamPath);
     });
 
     nms.run();
-  }
-
-  private transcodeToHLS(streamPath: string) {
-    const input = `rtmp://localhost:1935${streamPath}`;
-    const output = `./media/${streamPath.split('/').pop()}/index.m3u8`.replace(
-      '//',
-      '/',
-    );
-
-    const command = `ffmpeg -i ${input} -c:v libx264 -c:a aac -strict -2 -f hls -hls_time 10 -hls_list_size 5 -hls_flags delete_segments ${output}`;
-
-    // Executa o comando FFmpeg para gerar o HLS
-    exec(command, (error) => {
-      if (error) {
-        console.error(`Erro ao gerar HLS: ${error.message}`);
-        return;
-      }
-      console.log(`HLS gerado com sucesso: ${output}`);
-    });
   }
 }
